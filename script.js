e701b7c5dcba168fe51f2b630df3936ab96d0617
Khush Thakor <72325436+Khush205@users.noreply.github.com>
2021-08-12 16:40:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@
   var direction = "right";
   var lineLocation = 450;
   var w = 65;
+  var lineX = 0;
   ctx.lineWidth = 5;
   ctx.strokeRect(250,50,1000,w);
   ctx.lineWidth = 3;
   ctx.strokeRect(x,57,50,50);
-  var speed = 50;
+  randomLine(315,625);
   document.getElementById('startButton').addEventListener("click",move);
   document.getElementById('changeButton').addEventListener("click",change);
   document.getElementById('changeButton').style.visibility='hidden';
-  ctx.clearRect(0,0,canvas.length,canvas.heigth);
   function move(){
     document.getElementById('startButton').style.visibility='hidden';
     document.getElementById('changeButton').style.visibility='visible';
@@ -25,7 +25,7 @@
       if (x>=1185){
         gameOver();
       }
-    }, speed);
+    }, 20);
   }
   else{
   motion = setInterval(function () {
@@ -35,26 +35,40 @@
     if (x<=260){
       gameOver();
     }
-  },speed);
+  },20);
     }
+
 }
   function stop(){
     clearInterval(motion);
     moving = false;
   }
+
+  function randomLine(min,max){
+    let xC =  Math.random() * (max - min) + min;
+    lineX = xC;
+    drawLine(xC,50,xC,115);
+  }
+
   function change(){
     stop();
     moving = true;
     if (direction=="right"){
       direction = "left";
+      if(x>=lineX){
+      score++;
+    }
+      randomLine(335,605);
     }
     else{
       direction = "right";
+      if(x<=lineX){
+      score++;
+      }
+      randomLine(605,1115);
     }
     move();
-    score++;
     document.getElementById('score').innerHTML = "Score: "+score;
-    speed-=3;
   }
   function drawLine(x,y,x2,y2){
     ctx.lineWidth = 7;
